474bbda23c7adc02c2b7ba62308f1b1a4b35c23d
wangSh4913 <39670939+wangSh4913@users.noreply.github.com>
2020-12-05 21:29:25 +0800

    Update jiankong.go

--- client/jiankong.go
+++ client/jiankong.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
@@ -13,7 +14,7 @@ import (
 )
 
 var wg sync.WaitGroup
-var resMap = make(map[string]int, 1024)
+var resMap = make(map[int]map[string]int, 1024)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -28,7 +29,7 @@ func main() {
 	}
 	defer fileR.Close()
 	//打开写文件
-	fileW, err1 := os.OpenFile("result.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModePerm)
+	fileW, err1 := os.OpenFile("result.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 644)
 	if err1 != nil {
 		fmt.Println(err1)
 		return
@@ -48,42 +49,51 @@ func main() {
 		}
 		//去除每一行后面的\r\n
 		url := strings.TrimRight(line, "\r\n")
-		fmt.Printf("url is:%#v\n", url)
 		if len(url) == 0 {
 			continue
 		}
+		fmt.Printf("url is:%#v\n", url)
+		res := strings.Split(url, " ")
+		id, _ := strconv.ParseInt(res[0], 10, 64)
+		url = res[1]
+		fmt.Println("###############",id, url)
 		//链接输出，写到文件里
 		wg.Add(1)
-		go link(url)
+		go link(int(id), url)
 	}
 	wg.Wait()
 	fmt.Println(len(resMap))
-	for k, v := range resMap {
-		res := k + "  " + strconv.Itoa(v) + "\r\n"
-		writer.WriteString(res)
+	for id, m := range resMap {
+		for url, retCode := range m{
+			res := strconv.Itoa(id) + "  " + url + "  " + strconv.Itoa(retCode) + "\r\n"
+			writer.WriteString(res)
+		}
 	}
 	writer.Flush()
 }
 
 //尝试链接
-func link(url string) {
+func link(id int,url string) {
 	defer wg.Done()
 
+	tmp := make(map[string]int,1)
 	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 		url = "http://" + url
 	}
 
 	//设置超时为5s
-	client := http.Client{Timeout: 5 * time.Second}
+	client := http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		resMap[url] = -1
+		tmp[url] = -1
+		resMap[id] = tmp
 		return
 	}
 	if resp.StatusCode != 200 {
 		fmt.Printf("链接异常, url is:%s, statusCode is : %d, errInfo is :%s", url, resp.StatusCode, err)
-		resMap[url] = resp.StatusCode
+		tmp[url] = resp.StatusCode
+		resMap[id] = tmp
 	}
 	return
 }
